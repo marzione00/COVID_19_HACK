cb3db0da6a7cc3ecaf572f06635b8257e238be20
gregorio-saporito <59971032+gregorio-saporito@users.noreply.github.com>
2020-03-12 18:31:41 +0100

    shiny dashboard skeleton

---
+++ README.md
@@ -0,0 +1,24 @@
+# Covid_19 Rates Package
+
+The package has a Shiny app to explore the Covid-19 outbreak in Italy.
+
+## Installation
+
+```R
+# First install the R package "devtools" if not installed
+devtools::install_github('marzione00/COVID_19_HACK/tree/Gregorio')
+```
+
+## Usage
+
+Load the package
+
+```R
+require(Covid19)
+```
+
+The function of the package is `runC19()` and is run without arguments.
+
+```R
+runC19()
+```
---
+++ inst/shiny/C19/server.R
@@ -0,0 +1,44 @@
+## server.R ##
+
+server <- function(input, output, session) {
+  
+  shinyjs::runjs('
+        var el2 = document.querySelector(".skin-red");
+        el2.className = "skin-red sidebar-mini";
+        ')
+  
+  shinyjs::addClass(selector = "body", class = "sidebar-collapse")
+  
+  # section 1 ---------------------------------------------------------------
+  
+
+  # section 2 ---------------------------------------------------------------
+
+  waiter::waiter_hide()
+  
+  output$menu <- shinydashboard::renderMenu({
+    
+    shinydashboard::sidebarMenu(id="tabs",
+                                shinydashboard::menuItem("Home", tabName = "tab_1", icon=icon("home")),
+                                shinydashboard::menuItem("Inspection", tabName = "tab_2", icon=icon("search")),
+                                shinydashboard::menuItem("Analysis", tabName = "tab_3", icon=icon("chart-line")),
+                                shinydashboard::menuItem("Conclusion", tabName = "tab_4", icon=icon("calendar-check"))
+    )
+  })
+  
+  
+  # animations
+  observeEvent(input$tabs,{
+    shinyanimate::startAnim(session, 'effect_1', 'slideInUp')
+  })
+  observeEvent(input$tabs,{
+    shinyanimate::startAnim(session, 'effect_2', 'slideInUp')
+  })
+  observeEvent(input$tabs,{
+    shinyanimate::startAnim(session, 'effect_3', 'slideInUp')
+  })
+  observeEvent(input$tabs,{
+    shinyanimate::startAnim(session, 'effect_4', 'slideInUp')
+  })
+  
+}
