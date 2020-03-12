80c973e216fecf3253badbd7890187db80abcdbd
fabio130497 <60004596+fabio130497@users.noreply.github.com>
2020-03-12 14:58:25 +0100

    acq. dati da protezione civile
    first commit

---
+++ get_provTS.R
@@ -0,0 +1,13 @@
+get_provTS <- function() {
+  data <- read.csv("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province.csv",
+                   header = TRUE, na.strings = "In fase di definizione/aggiornamento")
+  
+  data$data <- as.POSIXct(data$data, format = "%Y-%m-%d %H", tz = "Europe/Berlin")
+  
+  out <- list()
+  for(province in unique(data$denominazione_provincia)) {
+    out[[as.character(province)]] <- subset(data, denominazione_provincia == province)
+  }
+  
+  return(out)
+}
