b405c8f7ad4c84c28f878ab498b6f6a003ddd024
fabio130497 <60004596+fabio130497@users.noreply.github.com>
2020-03-15 13:41:41 +0100

    AGGIORNAMENTO 5 - FABIO
    Aggiunto il plot delle differenze. Passo il testimone a Andrea per il cambio con plotly.

--- inst/shinyAPP/server.R
+++ inst/shinyAPP/server.R
@@ -67,12 +67,14 @@ server <- function(input, output, session) {
         shiny::sliderInput(inputId = "fitInterval", label = "Choose fitting interval",
                            min = init_date, max = fin_date, timeFormat = "%d %b",
                            step = 1, value = c(init_date, fin_date)),
-        shiny::uiOutput("filter_degree_region")
+        shiny::checkboxGroupInput(inputId = "plot_type", label = "Plot type",
+                                  choices = list("Cumulative cases" = 1, "New cases" = 2),
+                                  selected = 1)
       ),
       shiny::mainPanel(
         shiny::plotOutput("coolplot_region"), #plots that has to be loaded on the app
-        shiny::verbatimTextOutput("coolplot5_region"),
-        shiny::verbatimTextOutput("coolplot6_region")
+        shiny::verbatimTextOutput("fit_smry"),
+        shiny::verbatimTextOutput("chisq_smry")
       )
     )
   })
@@ -85,14 +87,20 @@ server <- function(input, output, session) {
   output$coolplot_region <- shiny::renderPlot({
 
     # Data trim and curve fitting
-    sample_date <- regionTS[[input$region]]$data_seriale
-    sample_cases <- regionTS[[input$region]]$totale_casi
     logic_interval <- regionTS[[input$region]]$data >= input$fitInterval[1] &
                       regionTS[[input$region]]$data <= input$fitInterval[2]
+
+    sample_date <- regionTS[[input$region]]$data_seriale
+    sample_cases <- regionTS[[input$region]]$totale_casi
+    sample_diff <-  c(NA,diff(sample_cases))
+
     sample_date_trim <- sample_date[logic_interval]
     sample_cases_trim <- sample_cases[logic_interval]
+    sample_diff_trim <- sample_diff[logic_interval]
+
     sample_date_rem <- sample_date[!logic_interval]
     sample_cases_rem <- sample_cases[!logic_interval]
+    sample_diff_rem <- sample_diff[!logic_interval]
 
     fit_data <- exe_fit(sample_cases = sample_cases_trim,
                         sample_date = sample_date_trim,
@@ -107,29 +115,51 @@ server <- function(input, output, session) {
                               sample_cases_trim)
     points_rem <- data.frame("sample_date_rem" = regionTS[[input$region]]$data[!logic_interval],
                              sample_cases_rem)
+    points_diff_trim <- data.frame("sample_date_trim" = regionTS[[input$region]]$data[logic_interval],
+                                   sample_diff_trim)
+    points_diff_rem <- data.frame("sample_date_rem" = regionTS[[input$region]]$data[!logic_interval],
+                                  sample_diff_rem)
+
     fittedPoints <- fit_data$fittedPoints
+    fittedPoints_der <- fit_data$fittedPoints_der
     seq_dates <- seq(from = init_date, by = 1, length.out = length(days))
     fittedPoints$days <- seq_dates
+    fittedPoints_der$days <- seq_dates
 
-    # Plot of fitted curve and points
-    ggplot2::ggplot(fittedPoints, ggplot2::aes_string("days","yFitted")) +
-    ggplot2::geom_line(color = "darkblue", size = 1)+
-    ggplot2::geom_point(data = points_rem, ggplot2::aes_string("sample_date_rem","sample_cases_rem"), size = 1, color="red")+
-    ggplot2::geom_point(data = points_trim, ggplot2::aes_string("sample_date_trim","sample_cases_trim"), size = 1, color="green")+
-    #geom_line(data=data_u,aes_string("days_dat","Conf_UP"),linetype = "dashed",size=1.05) +
-    #geom_line(data=data_u,aes_string("days_dat","Conf_DOWN"),linetype = "dashed",size=1.05) +
+    # Plot of fitted curve and points, conditional on checkbox input
+    if( 1 %in% input$plot_type ) {
+      ggplot2::ggplot(fittedPoints, ggplot2::aes_string("days","yFitted")) +
+        ggplot2::geom_line(color = "darkblue", size = 1)+
+        ggplot2::geom_point(data = points_rem, ggplot2::aes_string("sample_date_rem","sample_cases_rem"), size = 1, color="red")+
+        ggplot2::geom_point(data = points_trim, ggplot2::aes_string("sample_date_trim","sample_cases_trim"), size = 1, color="green")+
+        #geom_line(data=data_u,aes_string("days_dat","Conf_UP"),linetype = "dashed",size=1.05) +
+        #geom_line(data=data_u,aes_string("days_dat","Conf_DOWN"),linetype = "dashed",size=1.05) +
     ggplot2::theme_dark() +
-    ggplot2::theme(legend.position="top", panel.grid.minor = ggplot2::element_line(),text = ggplot2::element_text(size=14)) +
-    #ggplot2::scale_y_continuous(breaks=seq(0, 11/10*fit_data$out_fit$vals$k , 1/10*fit_data$out_fit$vals$k))+
-    ggplot2::labs(title="COVID19")
+      ggplot2::theme(legend.position="top", panel.grid.minor = ggplot2::element_line(),text = ggplot2::element_text(size=14)) +
+      #ggplot2::scale_y_continuous(breaks=seq(0, 11/10*fit_data$out_fit$vals$k , 1/10*fit_data$out_fit$vals$k))+
+      ggplot2::labs(title="COVID19")
+    }
+    if( 2 %in% input$plot_type ) {
+      ggplot2::ggplot(fittedPoints_der, ggplot2::aes_string("days","yFitted_der")) +
+        ggplot2::geom_line(color = "darkgreen", size = 1)+
+        ggplot2::geom_point(data = points_diff_rem, ggplot2::aes_string("sample_date_rem","sample_diff_rem"), size = 1, color="red")+
+        ggplot2::geom_point(data = points_diff_trim, ggplot2::aes_string("sample_date_trim","sample_diff_trim"), size = 1, color="green")+
+        #geom_line(data=data_u,aes_string("days_dat","Conf_UP"),linetype = "dashed",size=1.05) +
+        #geom_line(data=data_u,aes_string("days_dat","Conf_DOWN"),linetype = "dashed",size=1.05) +
+        ggplot2::theme_dark() +
+        ggplot2::theme(legend.position="top", panel.grid.minor = ggplot2::element_line(),text = ggplot2::element_text(size=14)) +
+        #ggplot2::scale_y_continuous(breaks=seq(0, 11/10*fit_data$out_fit$vals$k , 1/10*fit_data$out_fit$vals$k))+
+        ggplot2::labs(title="COVID19")
+    }
+
   })
 
-  output$coolplot5_region <- shiny::renderPrint(
+  output$fit_smry <- shiny::renderPrint(
     summary(reac_region$model)
     )
 
-  output$coolplot6_region <- shiny::renderPrint(
-    cat("Pvalue (chi-square) =",reac_region$chisq)    
+  output$chisq_smry <- shiny::renderPrint(
+    cat("Pvalue (chi-square) =",reac_region$chisq)
   )
 
   # output$coolplot_region <- renderText({
