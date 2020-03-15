48b3aae5dd6e5319546fe99ccff27bcc6b291cac
fabio130497 <60004596+fabio130497@users.noreply.github.com>
2020-03-15 19:47:28 +0100

    added global variables

--- inst/shiny/C19/server.R
+++ inst/shiny/C19/server.R
@@ -8,7 +8,10 @@ library(httr)
 
 server <- function(input, output, session) {
   
- 
+  countryTS <- get_countryTS()
+  regionTS <- get_regionTS()
+  provTS <- get_provTS()
+  
   source(file.path("server", "global/shinyjs.R"),  local = TRUE)$value
   
   source(file.path("server", "global/sidebar.R"),  local = TRUE)$value
