1525550685b8a07774404dc6d7644a5fed42fd12
Andrea Ierardi <andreierardi@gmail.com>
2020-03-16 17:58:17 +0100

    trying fix intensivecare.R

--- inst/shiny/C19/server/2module/intensivecare_cap.R
+++ inst/shiny/C19/server/2module/intensivecare_cap.R
@@ -1,12 +1,14 @@
 output$intensivecare_cap <- plotly::renderPlotly({
   
+    print(intensivecare_capacity)
 
     fig <- plot_ly(data = intensivecare_capacity, x =~region , y=~perc, type = 'bar',
                    marker = list(color = 'rgb(158,202,225)',
+                                 text = c(~capacity,~occupation),
                                  line = list(color = 'rgb(8,48,107)', width = 1.5)))
     fig <- fig %>% layout(
                           xaxis = list(title = "Region"),
-                          yaxis = list(title = "Intensive care capacity"))
+                          yaxis = list(title = "Percentage occupation/capacity"))
     
     fig
 })
