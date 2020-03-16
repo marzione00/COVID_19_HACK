1dda6ad38c558b5c43904df7faf06204d13b34f8
gregorio-saporito <59971032+gregorio-saporito@users.noreply.github.com>
2020-03-16 11:22:00 +0100

    map by province begin

--- inst/shiny/C19/server/1module/map.R
+++ inst/shiny/C19/server/1module/map.R
@@ -65,3 +65,31 @@
                                      joinBy = "id", value = "cases", name="total cases") %>%
       highcharter::hc_colorAxis(minColor = "#ffcc00", maxColor = "#cc0000")
   )
+  
+
+# map by province ---------------------------------------------------------
+
+  url <- "http://code.highcharts.com/mapdata/countries/it/it-all.geo.json"
+  tmpfile <- tempfile(fileext = ".json")
+  download.file(url, tmpfile)
+  
+  ita <- readLines(tmpfile)
+  
+  ita <- gsub(".* = ", "", ita)
+  ita <- jsonlite::fromJSON(ita, simplifyVector = FALSE)
+  
+  x <- ita$features[[1]]
+  x$properties
+  
+  dfita2 <-  ita$features %>% 
+    map_df(function(x){
+      data_frame(hasc = x$properties$hasc, name = x$properties$name)
+    }) %>%  # extract the keys
+    mutate(random = runif(nrow(.))) # create random value
+  
+  head(dfita2)
+  
+  highcharter::highchart(type = "map") %>% 
+    highcharter::hc_title(text = "Airports in Australia") %>% 
+    highcharter::hc_add_series_map(map = ita, df = dfita2, joinBy = "hasc", value = "random")
+  
