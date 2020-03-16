b51f483ea49345c770777d9bf56e88a9182aa899
Andrea Ierardi <andreierardi@gmail.com>
2020-03-16 16:39:08 +0100

    Update intensivecare_cap.R

--- inst/shiny/C19/server/2module/intensivecare_cap.R
+++ inst/shiny/C19/server/2module/intensivecare_cap.R
@@ -1,5 +1,20 @@
 output$intensivecare_cap <- plotly::renderPlotly({
   
-
   
-  })
+    intensivecare_capacity
+    
+    x <- c('Product A', 'Product B', 'Product C')
+    y <- c(20, 14, 23)
+    text <- c('27% market share', '24% market share', '19% market share')
+    data <- data.frame(x, y, text)
+    
+    fig <- plot_ly(data, x = ~x, y = ~y, type = 'bar',
+                   text = y, textposition = 'auto',
+                   marker = list(color = 'rgb(158,202,225)',
+                                 line = list(color = 'rgb(8,48,107)', width = 1.5)))
+    fig <- fig %>% layout(title = "January 2013 Sales Report",
+                          xaxis = list(title = ""),
+                          yaxis = list(title = ""))
+    
+    fig
+})
