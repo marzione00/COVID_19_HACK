5648eaa7db68bb409501f01c1f4d8bce19b068d0
fabio130497 <60004596+fabio130497@users.noreply.github.com>
2020-03-16 21:18:11 +0100

    added exe_fit and updated documentation

---
+++ R/exe_fit.R
@@ -0,0 +1,43 @@
+#' Logistic curve fitting
+#'
+#' @param sample_cases values for growth fitting.
+#' @param sample_date sample dates as serial consecutive dates (should be included in \code{days}).
+#' @param days full vector of serial dates.
+#'
+#' @return a list of fitting results. Specifically:
+#' \describe{
+#'   \item{out_fit}{The output of the fitting performed by \link[growthcurver]{SummarizeGrowth}.}
+#'   \item{fittedPoints}{A data frame containing dates and corresponding fitted values of cumulative cases, listed by columns.}
+#'   \item{out_chisq}{The output of the chi-squared independence test of residuals.}
+#'   \item{fittedPoints_der}{A data frame containing dates and the corresponding fitted values of new cases, listed by columns.}
+#' }
+#'
+#' @examples
+#' \dontrun{
+#'   data <- get_regionTS()
+#'   data <- subset(data, denominazione_regione == "Lombardia")
+#'   exe_fit(data$totale_casi, data$data_seriale, c(1:50))
+#' }
+#'
+#' @export
+exe_fit <- function(sample_cases, sample_date, days) {
+  # Curve fitting
+  out_fit <- growthcurver::SummarizeGrowth(sample_date, sample_cases)
+  k=out_fit[["vals"]][["k"]]
+  n0=out_fit[["vals"]][["n0"]]
+  r=out_fit[["vals"]][["r"]]
+
+  # Creation of fitted points
+  yFitted <- (n0*k)/(n0 + (k-n0) * exp(-r*days))
+  yFitted_der <- (k*n0*r*(k - n0)*exp(-r*days))/((k - n0)*exp(-r*days) + n0)^2
+  fittedPoints <- data.frame(days,yFitted)
+  fittedPoints_der <- data.frame(days,yFitted_der)
+
+  # Chi-squared test
+  yFitted_chi <- (n0*k)/(n0 + (k-n0) * exp(-r*sample_date))
+  output_chisquared <- stats::chisq.test(sample_cases,yFitted_chi)
+
+  output_fin <- list("out_fit" = out_fit, "fittedPoints" = fittedPoints,
+                     "out_chisq" = output_chisquared, "fittedPoints_der" = fittedPoints_der)
+  return(output_fin)
+}
--- inst/shiny/C19/server/3module/analysis.R
+++ inst/shiny/C19/server/3module/analysis.R
@@ -15,15 +15,136 @@ days <- (1:50)
 
 output$regionPanel <- shiny::renderUI({
   shinydashboard::dashboardBody(
-    box(title = "Input", 
-        shiny::selectInput(inputId = "region", label = "Choose one region",
-                           choices = regNames),
-        shiny::sliderInput(inputId = "fitInterval", label = "Choose fitting interval",
-                           min = init_date, max = fin_date, timeFormat = "%d %b",
-                           step = 1, value = c(init_date, fin_date)),
-        shiny::checkboxGroupInput(inputId = "plot_type", label = "Plot type",
-                                  choices = list("Cumulative cases" = 1, "New cases" = 2),
-                                  selected = 1)),
-    box(title = "Charts", plotly::plotlyOutput("coolplot_region"))
+    waiter::use_waiter(),
+    waiter::waiter_show(id = "coolplot_region", html = waiter::spin_fading_circles()),
+    shiny::fluidRow(
+      shinydashboard::box(title = "Input", 
+                          shiny::selectInput(inputId = "region", label = "Choose one region",
+                                             choices = regNames),
+                          shiny::sliderInput(inputId = "fitInterval", label = "Choose fitting interval",
+                                             min = init_date, max = fin_date, timeFormat = "%d %b",
+                                             step = 1, value = c(init_date, fin_date)),
+                          shiny::checkboxGroupInput(inputId = "plot_type", label = "Plot type",
+                                                    choices = list("Cumulative cases" = 1, "New cases" = 2),
+                                                    selected = 1),
+                          width = 4),
+      shinydashboard::box(title = "Charts", plotly::plotlyOutput("coolplot_region"),
+                          width = 8)
+    ),
+    shiny::fluidRow(
+      shinydashboard::tabBox(
+        title = "Technical data", id = "tech_tab",
+        shiny::tabPanel("Fitting output", shiny::verbatimTextOutput("fit_smry")),
+        shiny::tabPanel("Chi-squared test", shiny::verbatimTextOutput("chisq_smry"))
+      )
+    )
+      
   )
 })
+
+
+## REGION reactive values ##
+reac_region <- shiny::reactiveValues()
+
+## REGION plot (currently date against total cases) ##
+output$coolplot_region <- plotly::renderPlotly({
+  
+  # Data trim and curve fitting #
+  logic_interval <- regionTS[[input$region]]$data >= input$fitInterval[1] &
+    regionTS[[input$region]]$data <= input$fitInterval[2]
+  
+  sample_date <- regionTS[[input$region]]$data_seriale
+  sample_cases <- regionTS[[input$region]]$totale_casi
+  sample_diff <-  c(NA,diff(sample_cases))
+  
+  sample_date_trim <- sample_date[logic_interval]
+  sample_cases_trim <- sample_cases[logic_interval]
+  sample_diff_trim <- sample_diff[logic_interval]
+  
+  sample_date_rem <- sample_date[!logic_interval]
+  sample_cases_rem <- sample_cases[!logic_interval]
+  sample_diff_rem <- sample_diff[!logic_interval]
+  
+  fit_data <- exe_fit(sample_cases = sample_cases_trim,
+                      sample_date = sample_date_trim,
+                      days = days)
+  
+  reac_region$model <- fit_data$out_fit$model
+  reac_region$chisq <- fit_data$out_chisq$p.value
+  reac_region$vals <- fit_data$out_fit$vals
+  
+  # Conversion to real date and creation of fitted points #
+  points_trim <- data.frame("sample_date_trim" = regionTS[[input$region]]$data[logic_interval],
+                            sample_cases_trim)
+  points_rem <- data.frame("sample_date_rem" = regionTS[[input$region]]$data[!logic_interval],
+                           sample_cases_rem)
+  points_diff_trim <- data.frame("sample_date_trim" = regionTS[[input$region]]$data[logic_interval],
+                                 sample_diff_trim)
+  points_diff_rem <- data.frame("sample_date_rem" = regionTS[[input$region]]$data[!logic_interval],
+                                sample_diff_rem)
+  
+  fittedPoints <- fit_data$fittedPoints
+  fittedPoints_der <- fit_data$fittedPoints_der
+  seq_dates <- seq(from = init_date, by = 1, length.out = length(days))
+  fittedPoints$days <- seq_dates
+  fittedPoints_der$days <- seq_dates
+  
+  
+  # PLOT with plotly #
+  fig = plotly::plot_ly( name = "Cases", type= "scatter")
+  
+  # funtions for the two different plots
+  plot1  = function(fig)
+  {
+    fig <- fig %>% plotly::add_trace(data = fittedPoints, x = ~days, y = ~yFitted, line = list(color ='rgb(0,0,139)',width=4), mode='lines', name = "Fitted logistic curve”" )
+    
+    fig <- fig %>% plotly::add_trace(data =  points_rem, x =~sample_date_rem, y =~sample_cases_rem ,marker = list(color = "red"), mode = 'markers', name = "Total cases (excluded)")
+    fig <- fig %>% plotly::add_trace(data = points_trim, x =~sample_date_trim, y =~sample_cases_trim ,marker = list(color = "green"), mode = 'markers', name = "Total cases (fitting)")
+    return(fig)
+  }
+  
+  plot2 = function (fig)
+  {
+    fig <- fig %>% plotly::add_trace(data = fittedPoints_der, x = ~days, y = ~yFitted_der, line = list(color ='rgb(255,117,20)',width=4), mode='lines', name= "Fitted logistic distribution")
+    
+    fig <- fig %>% plotly::add_bars(data =  points_diff_rem, x =~sample_date_rem, y =~sample_diff_rem, marker = list(color = "red"), name = "New cases (excluded)")
+    fig <- fig %>% plotly::add_bars(data =  points_diff_trim, x =~sample_date_trim, y =~sample_diff_trim, marker = list(color = "green"), name = "New cases (fitting)")
+    
+    return(fig)
+  }
+  
+  #Plot based on the checkbox
+  if( 1 %in% input$plot_type && !(2 %in% input$plot_type ) )
+  {
+    fig = plot1(fig)
+  } else if( 2 %in% input$plot_type && !(1 %in% input$plot_type ) )
+  {
+    fig = plot2(fig)
+  } else if( 1 %in% input$plot_type && (2 %in% input$plot_type ) )
+  {
+    fig = plot1(fig)
+    fig = plot2(fig)
+  }
+  
+  #plot
+  fig
+  
+})
+
+
+# OUTPUT SUMMARY HTML --- TODO
+# output$fit_smryhtml <- shiny::renderText({
+#
+#   #text = HTML(paste0("<b>","Summary","</b><br>",  summary(reac_region$model)))
+#   text = HTML(paste0(as.character(summary(reac_region$model))))
+#   text
+#
+# })
+
+output$fit_smry <- shiny::renderPrint(
+  summary(reac_region$model)
+)
+
+output$chisq_smry <- shiny::renderPrint(
+  cat("Pvalue (chi-square) =",reac_region$chisq)
+)
