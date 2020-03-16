87730a96caa1327b7bc8ec91fc436d78dfe17eb8
Andrea Ierardi <andreierardi@gmail.com>
2020-03-16 18:09:11 +0100

    Update intensivecare_cap.R

--- inst/shiny/C19/server/2module/intensivecare_cap.R
+++ inst/shiny/C19/server/2module/intensivecare_cap.R
@@ -2,9 +2,8 @@ output$intensivecare_cap <- plotly::renderPlotly({
   
     print(intensivecare_capacity)
 
-    fig <- plot_ly(data = intensivecare_capacity, x =~region , y=~perc, type = 'bar',
-                   marker = list(color = 'rgb(158,202,225)',
-                                 text = c(~capacity,~occupation),
+    fig <- plot_ly(data = intensivecare_capacity, x =~region , y=~perc, type = 'bar', 
+                   marker = list(color = 'rgb(255,250,205)',
                                  line = list(color = 'rgb(8,48,107)', width = 1.5)))
     fig <- fig %>% layout(
                           xaxis = list(title = "Region"),
