73c7512037559346418cce67dd91daf284346b87
Andrea Ierardi <andreierardi@gmail.com>
2020-03-19 17:44:13 +0100

    fix module2

--- R/get_intesivecare_cap.R
+++ R/get_intesivecare_cap.R
@@ -17,7 +17,7 @@ get_intensivecare_cap <- function(regionTS) {
   #readfile  <- read.csv(system.file("extdata", "Posti_TI.csv", package="covid19"),header=TRUE)
   colnames(readfile) = c("region","capacity")
   readfile = readfile[order(readfile$region),]
-
+  
   occupation =c()
  
   sortedreg = regionTS[order(names(regionTS))]
@@ -28,6 +28,7 @@ get_intensivecare_cap <- function(regionTS) {
     occupation[index_region] = tail(vector, n = 1)
     
   }
+
   df = data.frame(readfile,occupation)
   colnames(df)= c("region","capacity","occupation")
   perc = c(as.integer(df$occupation/df$capacity*100))
