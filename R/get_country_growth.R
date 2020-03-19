6f8ea21024ae0bb75681148f9f6e287bceb0dec1
gregorio-saporito <59971032+gregorio-saporito@users.noreply.github.com>
2020-03-19 15:07:36 +0100

    get_country_growth fix

--- R/get_country_growth.R
+++ R/get_country_growth.R
@@ -1,14 +1,11 @@
-#' Data acquisition
-#'
-#' @return the national dataset
+#' Data manipulation
 #'
 #' @export
-get_countryTS <- function() {
-  data <- read.csv("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv",
-                   header = TRUE)
-
-  data$data <- as.Date(as.character(data$data), format = "%Y-%m-%d")
-  data["data_seriale"] = c(1:length(data$data)-1)
-  return(data)
+get_country_growth <- function() {
+  
+  get_countryTS() %>%
+    select(totale_casi) %>%
+    mutate(growth=round(((totale_casi-lag(totale_casi))/lag(totale_casi))*100,2) ) %>%
+    mutate(growth_change=round(((growth-lag(growth))/lag(growth))*100,2) )
 
 }
