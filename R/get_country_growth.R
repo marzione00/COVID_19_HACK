00853de88ecfa359a1e74f7b593c4d59bc85148a
gregorio-saporito <59971032+gregorio-saporito@users.noreply.github.com>
2020-03-24 12:55:57 +0100

    growth monitoring delta fix

--- R/get_country_growth.R
+++ R/get_country_growth.R
@@ -6,6 +6,6 @@ get_country_growth <- function() {
   countryTS %>%
     dplyr::select(totale_casi) %>%
     dplyr::mutate(growth=round(((totale_casi-dplyr::lag(totale_casi))/dplyr::lag(totale_casi))*100,2) ) %>%
-    dplyr::mutate(growth_change=round(((growth-dplyr::lag(growth))/dplyr::lag(growth))*100,2) )
+    dplyr::mutate(growth_change=round(growth-dplyr::lag(growth),2) )
 
 }
