7e6954dd4286e7322b0fcb2cd373aaef426d82b7
Andrea Ierardi <andreierardi@gmail.com>
2020-03-25 18:43:53 +0100

    upload

--- R/const_trim.R
+++ R/const_trim.R
@@ -8,7 +8,7 @@
 #'
 #' @return the sliced initial vector
 #'
-#' @example
+#' @examples
 #' \dontrun{
 #' 	const_trim(c(0,0,0,1,1.3,1.7,2,3,4.5,6,9,12),1)
 #' }
--- R/intensivecare_bed_datas.R
+++
@@ -1,5 +0,0 @@
-#' Dataset intensive care region beds
-#' 
-#' @format tibble
-#' @export
-"intensivecare_beds"
