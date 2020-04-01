064071f25568d94e25548e3c6a619bfb605a8d42
gregorio-saporito <59971032+gregorio-saporito@users.noreply.github.com>
2020-04-01 14:45:48 +0200

    fictitious tab add home ui

--- inst/shiny/C19/server/global/sidebar.R
+++ inst/shiny/C19/server/global/sidebar.R
@@ -1,7 +1,7 @@
 # sidebard ----------------------------------------------------------------
 output$menu <-   shinydashboard::renderMenu({
   shinydashboard::sidebarMenu(id="tabs",
-            shinydashboard::menuItem("", tabName = ""),
+            shinydashboard::menuItem("", tabName = "home"),
             shinydashboard::menuItem("Home", tabName = "home", icon=icon("home")),
             shinydashboard::menuItem("Inspection", tabName = "tab_2", icon=icon("search")),
             shinydashboard::menuItem("Analysis", tabName = "tab_3", icon=icon("chart-line")),
