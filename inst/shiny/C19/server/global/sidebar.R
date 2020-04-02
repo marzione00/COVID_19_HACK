95247be3cde950c9300bdbd1f1d7043fe8714a0b
Andreaierardi <andreierardi@gmail.com>
2020-04-02 11:00:11 +0200

    fixed fake tab

--- inst/shiny/C19/server/global/sidebar.R
+++ inst/shiny/C19/server/global/sidebar.R
@@ -1,7 +1,8 @@
 # sidebard ----------------------------------------------------------------
 output$menu <-   shinydashboard::renderMenu({
   shinydashboard::sidebarMenu(id="tabs",
-            shinydashboard::menuItem("", tabName = "home"),
+                              tags$div(class = "hide", checked = NA,
+            shinydashboard::menuItem("", tabName = "home")),
             shinydashboard::menuItem("Home", tabName = "home", icon=icon("home")),
             shinydashboard::menuItem("Inspection", tabName = "tab_2", icon=icon("search")),
             shinydashboard::menuItem("Analysis", tabName = "tab_3", icon=icon("chart-line")),
