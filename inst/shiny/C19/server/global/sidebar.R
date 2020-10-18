443904b4b8fd7540e425c4e2ab302fa3ef6a3b3f
Gregorio Saporito <59971032+gregorio-saporito@users.noreply.github.com>
2020-10-18 14:49:26 +0200

    Map as a section of inspection

--- inst/shiny/C19/server/global/sidebar.R
+++ inst/shiny/C19/server/global/sidebar.R
@@ -4,8 +4,10 @@ output$menu <-   shinydashboard::renderMenu({
                               tags$div(class = "hide", checked = NA,
             shinydashboard::menuItem("", tabName = "home")),
             shinydashboard::menuItem("Home", tabName = "home", icon=icon("home")),
-            shinydashboard::menuItem("Map", tabName = "map", icon=icon("map-marked-alt")),
-            shinydashboard::menuItem("Inspection", tabName = "tab_2", icon=icon("search")),
+            shinydashboard::menuItem("Inspection", tabName = "inspection", icon=icon("search"),
+                                     shinydashboard::menuSubItem("Section 1", tabName = "map"),
+                                     shinydashboard::menuSubItem("Secion 2", tabName = "tab_2")
+            ),
             shinydashboard::menuItem("Analysis", tabName = "tab_3_all", icon=icon("chart-line"),
                                      shinydashboard::menuSubItem("Section 1", tabName = "tab_3"),
                                      shinydashboard::menuSubItem("Secion 2", tabName = "tab_3_2")
