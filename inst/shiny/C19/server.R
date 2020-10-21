65cb34d8b45e15d5883fe357e885611399bf3a59
Andreaierardi <andreierardi@gmail.com>
2020-10-21 18:38:34 +0200

    update background and cookies

--- inst/shiny/C19/server.R
+++ inst/shiny/C19/server.R
@@ -22,7 +22,8 @@ server <- function(input, output, session) {
   
   
   shinyalert::shinyalert(imageWidth=100,imageUrl="https://cdn1.vectorstock.com/i/1000x1000/68/55/data-analysis-round-vector-7826855.jpg",animation="slide-from-top","Welcome!", 
-                         "You can navigate through the app easily through the panels on the left and the scrollbars.\n\nNB. All plots are INTERACTIVE: draw a square to zoom in and take full advantage of dynamic tools!", type = "success")
+                         HTML("You can navigate through the app easily through the panels on the left and the scrollbars.\n\nNB. All plots are INTERACTIVE: draw a square to zoom in and take full advantage of dynamic tools! \n\n\n ====  Notice ==== \n We and selected partners, use cookies or similar technologies as specified in the cookie policy.
+You can consent to the use of such technologies by closing this notice, by scrolling this page, by interacting with any link or button outside of this notice or by continuing to browse otherwise."), type = "success")
   
 }
 
